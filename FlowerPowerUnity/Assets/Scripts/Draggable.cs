dab49fa06855ba8e6945d945454ff6b29257b2c8
malcolmbec <35633689+malcolmbec@users.noreply.github.com>
2020-10-04 11:32:38 -0400

    drag object

---
+++ FlowerPowerUnity/Assets/Scripts/Draggable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IDragHandler
+{
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+
+
+}
