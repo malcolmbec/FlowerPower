98fec336d9baafbafeed6492152fcc1e9230e2e8
malcolmbec <35633689+malcolmbec@users.noreply.github.com>
2020-10-04 01:53:27 -0400

    import yarn pkg

---
+++ FlowerPowerUnity/Assets/Scripts/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class Customer : MonoBehaviour
+{
+    public YarnProgram script;
+    public DialogueRunner dialogueRunner;
+
+    void Start()
+    {
+        dialogueRunner.Add(script);
+    }
+
+}
