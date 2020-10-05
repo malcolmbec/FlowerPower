297e44d19676b8028836538b212314cfee4f53bd
Brent Oakes <boakes@users.noreply.github.com>
2020-10-05 14:58:50 -0500

    First Push

--- FlowerPowerUnity/Assets/Scripts/ChangeScene.cs
+++ FlowerPowerUnity/Assets/Scripts/ChangeScene.cs
@@ -5,16 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    public GameObject ShopScene;
+
+    public void Start()
+    {
+        ShopScene = GameObject.FindGameObjectWithTag("ShopScene");
+    }
+
     public void GoToShop()
     {
-        SceneManager.LoadScene("ShopScene");
+        SceneManager.UnloadSceneAsync("FlowerScene");
+        //SceneManager.LoadScene("ShopScene");
         //SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(0));
+        ShopScene.SetActive(true);
     }
 
     public void GoToFlower()
     {
-        SceneManager.LoadScene("FlowerScene");
+        SceneManager.LoadScene("FlowerScene", LoadSceneMode.Additive);
         //SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(1));
+        
+        //We should check to make sure scene is loaded, but for now, we're gonna risk it
+        ShopScene.SetActive(false);
+        
     }
 
 }
---
+++ FlowerPowerUnity/Assets/Scripts/GeneralManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+//usage:
+public class GeneralManager : MonoBehaviour
+{
+    //TODO: Maybe utilize the lazy singleton we've seen. That will be 
+    //Thread safe, but we shouldn't have an issue with this one anyways...
+
+    private static GeneralManager _gm;
+
+    public static GeneralManager Gm {get { return _gm; }}
+
+    //public GameObject currentPatron;
+    public GameObject currentFlowers;
+    public GameObject vase;
+
+
+
+    void Awake() {
+        if (_gm != null && _gm != this) {
+            Destroy(this.gameObject);
+        } else {
+            _gm = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+
+}
