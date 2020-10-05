466cd5fc443ac5d60e9f67cc739f16c8b216ce14
malcolmbec <35633689+malcolmbec@users.noreply.github.com>
2020-10-05 13:05:33 -0400

    Type note in Flower scene

--- FlowerPowerUnity/Assets/Scripts/ChangeScene.cs
+++ FlowerPowerUnity/Assets/Scripts/ChangeScene.cs
@@ -5,22 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-    private void Awake()
-    {
-        // SceneManager.LoadScene("FlowerScene", LoadSceneMode.Additive);
-        // SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(0));
-    }
-
     public void GoToShop()
     {
-        SceneManager.LoadScene("ShopScene");
-        // SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(0));
+        //SceneManager.LoadScene("ShopScene");
+        SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(0));
     }
 
     public void GoToFlower()
     {
-        SceneManager.LoadScene("FlowerScene");
-        // SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(1));
+        //SceneManager.LoadScene("FlowerScene");
+        SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(1));
     }
 
 }
