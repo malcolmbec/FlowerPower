b0baf7f6c77f8c3f147324d0853dac89a3488829
malcolmbec <35633689+malcolmbec@users.noreply.github.com>
2020-10-04 20:06:56 -0400

    additive scene tbd

--- FlowerPowerUnity/Assets/Scripts/ChangeScene.cs
+++ FlowerPowerUnity/Assets/Scripts/ChangeScene.cs
@@ -5,14 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    private void Awake()
+    {
+        // SceneManager.LoadScene("FlowerScene", LoadSceneMode.Additive);
+        // SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(0));
+    }
+
     public void GoToShop()
     {
         SceneManager.LoadScene("ShopScene");
+        // SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(0));
     }
 
     public void GoToFlower()
     {
         SceneManager.LoadScene("FlowerScene");
+        // SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(1));
     }
 
 }
